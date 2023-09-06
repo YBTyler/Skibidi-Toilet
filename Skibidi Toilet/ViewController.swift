79cd913c40509cc7c15a9fcdb8b5e4827003383b
Tyler Wickherst <miketylerwickherst@gmail.com>
2023-09-05 22:16:29 -0500

    Initial Commit

---
+++ Skibidi Toilet/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Skibidi Toilet
+//
+//  Created by Tyler Wickherst on 9/5/23.
+//
+
+import UIKit
+import AVFoundation
+import UserNotifications
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+
+    @IBAction func skibidiButton(_ sender: Any) {
+       skibidiSound()
+    
+      
+        func viewDidLoad() 
+        {
+            super.viewDidLoad()
+            
+        }
+    }
+    func skibidiSound(){
+        let url = Bundle.main.url(forResource:"skibi", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf:url!)
+        player!.play()
+    }
+}
+
